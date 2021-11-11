Icinga/icinga-go-library

commit 70fb27dc2f265d8d1d17ac2c4858bcc79e3ad98e

Don't log if context is canceled

retry/retry.go
@@ -48,6 +48,10 @@ func WithBackoff(
 			return
 		}
 
+		if err = parentCtx.Err(); err != nil {
+			return
+		}
+
 		if settings.OnError != nil {
 			settings.OnError(time.Since(start), attempt, err, prevErr)
 		}

commit 906e51b294bc558badfdf14e8c6e63381afe38bb

Fix broken retry mechanics
70fb27d introduced a parent context check to suppress logging if
it was canceled.
If it was not canceled, the check overwrites the variable err,
which resets every previous real error to nil,
which then leads to fatals because the OnError callback or
the IsRetryable function expect a non-nil error.
Since I cannot reproduce which logs should have been suppressed by
the changes, I removed them completely.

retry/retry.go
@@ -48,10 +48,6 @@ func WithBackoff(
 			return
 		}
 
-		if err = parentCtx.Err(); err != nil {
-			return
-		}
-
 		if settings.OnError != nil {
 			settings.OnError(time.Since(start), attempt, err, prevErr)
 		}