c9b47f8430cd4d3b109db4a4710aeebc8f28c5b4
Eric Lippmann <eric.lippmann@icinga.com>
2024-04-25 19:28:38 +0200

    Fix `types#UnixMilli.UnmarshalText()` crashing on empty strings

--- types/unix_milli_test.go
+++ types/unix_milli_test.go
@@ -13,9 +13,9 @@ func TestUnixMilli_MarshalJSON(t *testing.T) {
 		input  UnixMilli
 		output string
 	}{
-		{"zero", UnixMilli{}, `null`},
-		{"epoch", UnixMilli(time.Unix(0, 0)), `0`},
-		{"nonzero", UnixMilli(time.Unix(1234567890, 62500000)), `1234567890062`},
+		{"zero", UnixMilli{}, "null"},
+		{"epoch", UnixMilli(time.Unix(0, 0)), "0"},
+		{"nonzero", UnixMilli(time.Unix(1234567890, 62000000)), "1234567890062"},
 	}
 
 	for _, st := range subtests {
@@ -25,6 +25,72 @@ func TestUnixMilli_MarshalJSON(t *testing.T) {
 			require.NoError(t, err)
 			require.True(t, utf8.Valid(actual))
 			require.Equal(t, st.output, string(actual))
+		})
+	}
+}
+
+func TestUnixMilli_UnmarshalJSON(t *testing.T) {
+	subtests := []struct {
+		name   string
+		input  UnixMilli
+		output string
+	}{
+		{"zero", UnixMilli{}, "null"},
+		{"epoch", UnixMilli(time.Unix(0, 0)), "0"},
+		{"nonzero", UnixMilli(time.Unix(1234567890, 62000000)), "1234567890062"},
+	}
+
+	for _, st := range subtests {
+		t.Run(st.name, func(t *testing.T) {
+			var s UnixMilli
+			err := s.UnmarshalJSON([]byte(st.output))
+
+			require.NoError(t, err)
+			require.Equal(t, st.input, s)
+		})
+	}
+}
+
+func TestUnixMilli_MarshalText(t *testing.T) {
+	subtests := []struct {
+		name   string
+		input  UnixMilli
+		output string
+	}{
+		{"zero", UnixMilli{}, ""},
+		{"epoch", UnixMilli(time.Unix(0, 0)), "0"},
+		{"nonzero", UnixMilli(time.Unix(1234567890, 62000000)), "1234567890062"},
+	}
+
+	for _, st := range subtests {
+		t.Run(st.name, func(t *testing.T) {
+			actual, err := st.input.MarshalText()
+
+			require.NoError(t, err)
+			require.True(t, utf8.Valid(actual))
+			require.Equal(t, st.output, string(actual))
+		})
+	}
+}
+
+func TestUnixMilli_UnmarshalText(t *testing.T) {
+	subtests := []struct {
+		name   string
+		input  UnixMilli
+		output string
+	}{
+		{"zero", UnixMilli{}, ""},
+		{"epoch", UnixMilli(time.Unix(0, 0)), "0"},
+		{"nonzero", UnixMilli(time.Unix(1234567890, 62000000)), "1234567890062"},
+	}
+
+	for _, st := range subtests {
+		t.Run(st.name, func(t *testing.T) {
+			var s UnixMilli
+			err := s.UnmarshalText([]byte(st.output))
+
+			require.NoError(t, err)
+			require.Equal(t, st.input, s)
 		})
 	}
 }
