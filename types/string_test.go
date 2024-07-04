49d04b5c78d94ad6dfeaa4aebe9942bc4c1fe779
Alexander A. Klimov <alexander.klimov@icinga.com>
2024-07-04 17:38:09 +0200

    Test types.String#MarshalJSON()

--- types/string_test.go
+++ types/string_test.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"github.com/stretchr/testify/require"
 	"testing"
+	"unicode/utf8"
 )
 
 func TestMakeString(t *testing.T) {
@@ -20,6 +21,31 @@ func TestMakeString(t *testing.T) {
 	for _, st := range subtests {
 		t.Run(st.name, func(t *testing.T) {
 			require.Equal(t, String{NullString: sql.NullString{String: st.io, Valid: true}}, MakeString(st.io))
+		})
+	}
+}
+
+func TestString_MarshalJSON(t *testing.T) {
+	subtests := []struct {
+		name   string
+		input  sql.NullString
+		output string
+	}{
+		{"null", sql.NullString{}, `null`},
+		{"invalid", sql.NullString{String: "abc"}, `null`},
+		{"empty", sql.NullString{String: "", Valid: true}, `""`},
+		{"nul", sql.NullString{String: "\x00", Valid: true}, `"\u0000"`},
+		{"space", sql.NullString{String: " ", Valid: true}, `" "`},
+		{"multiple", sql.NullString{String: "abc", Valid: true}, `"abc"`},
+	}
+
+	for _, st := range subtests {
+		t.Run(st.name, func(t *testing.T) {
+			actual, err := String{st.input}.MarshalJSON()
+
+			require.NoError(t, err)
+			require.True(t, utf8.Valid(actual))
+			require.Equal(t, st.output, string(actual))
 		})
 	}
 }
