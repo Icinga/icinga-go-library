699afd76ff43f183809edf675bc6900786e56c39
Alexander A. Klimov <alexander.klimov@icinga.com>
2024-07-05 14:52:11 +0200

    Test types.Bool#UnmarshalText()

--- types/bool_test.go
+++ types/bool_test.go
@@ -28,3 +28,33 @@ func TestBool_MarshalJSON(t *testing.T) {
 		})
 	}
 }
+
+func TestBool_UnmarshalText(t *testing.T) {
+	subtests := []struct {
+		name   string
+		input  string
+		output Bool
+		error  bool
+	}{
+		{"empty", "", Bool{}, true},
+		{"negative", "-1", Bool{}, true},
+		{"bool", "false", Bool{}, true},
+		{"b", "f", Bool{}, true},
+		{"float", "0.0", Bool{}, true},
+		{"zero", "0", Bool{Bool: false, Valid: true}, false},
+		{"one", "1", Bool{Bool: true, Valid: true}, false},
+		{"two", "2", Bool{Bool: true, Valid: true}, false},
+	}
+
+	for _, st := range subtests {
+		t.Run(st.name, func(t *testing.T) {
+			var actual Bool
+			if err := actual.UnmarshalText([]byte(st.input)); st.error {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, st.output, actual)
+			}
+		})
+	}
+}
