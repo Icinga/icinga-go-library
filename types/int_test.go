d8b9cb688301e72b60060a7b743aa4536a09693e
Alexander A. Klimov <alexander.klimov@icinga.com>
2024-07-05 11:07:32 +0200

    Test types.Int#UnmarshalText()

--- types/int_test.go
+++ types/int_test.go
@@ -27,3 +27,31 @@ func TestInt_MarshalJSON(t *testing.T) {
 		})
 	}
 }
+
+func TestInt_UnmarshalText(t *testing.T) {
+	subtests := []struct {
+		name   string
+		input  string
+		output sql.NullInt64
+		error  bool
+	}{
+		{"empty", "", sql.NullInt64{}, true},
+		{"2p64", "18446744073709551616", sql.NullInt64{}, true},
+		{"float", "0.0", sql.NullInt64{}, true},
+		{"zero", "0", sql.NullInt64{Int64: 0, Valid: true}, false},
+		{"negative", "-1", sql.NullInt64{Int64: -1, Valid: true}, false},
+		{"2p62", "4611686018427387904", sql.NullInt64{Int64: 1 << 62, Valid: true}, false},
+	}
+
+	for _, st := range subtests {
+		t.Run(st.name, func(t *testing.T) {
+			var actual Int
+			if err := actual.UnmarshalText([]byte(st.input)); st.error {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, Int{NullInt64: st.output}, actual)
+			}
+		})
+	}
+}
