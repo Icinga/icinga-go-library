cc44c3f1eb7e7229a507d533d5d2b25537684915
Alexander A. Klimov <alexander.klimov@icinga.com>
2024-07-08 15:14:02 +0200

    Test com.Atomic#Swap()

--- com/atomic_test.go
+++ com/atomic_test.go
@@ -41,3 +41,32 @@ func TestAtomic_Load(t *testing.T) {
 		})
 	}
 }
+
+func TestAtomic_Swap(t *testing.T) {
+	subtests := []struct {
+		name string
+		init bool
+		io   testInterface
+		new  testInterface
+	}{
+		{"uninitialized", false, nil, (*testImpl)(nil)},
+		{"nil", true, (*testImpl)(nil), nil},
+	}
+
+	for _, st := range subtests {
+		t.Run(st.name, func(t *testing.T) {
+			var a Atomic[testInterface]
+			if st.init {
+				a.Store(st.io)
+			}
+
+			old, ok := a.Swap(st.new)
+			require.Equal(t, st.init, ok, "Swap second return value")
+			require.Equal(t, st.io, old, "Swap first return value")
+
+			v, ok := a.Load()
+			require.True(t, ok, "Load second return value")
+			require.Equal(t, st.new, v, "Load first return value")
+		})
+	}
+}
