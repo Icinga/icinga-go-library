9590392c1bb0aa64aecd3f9a9f822ddf23f4b34a
Alexander A. Klimov <alexander.klimov@icinga.com>
2024-07-19 17:27:08 +0200

    Test com.Counter#Reset()

--- com/counter_test.go
+++ com/counter_test.go
@@ -16,3 +16,17 @@ func TestCounter_Add(t *testing.T) {
 	require.Equal(t, uint64(65), c.Val(), "unexpected new value")
 	require.Equal(t, uint64(65), c.Total(), "unexpected new total")
 }
+
+func TestCounter_Reset(t *testing.T) {
+	var c Counter
+
+	c.Add(42)
+	c.Reset()
+
+	require.Equal(t, uint64(0), c.Val(), "unexpected value")
+	require.Equal(t, uint64(42), c.Total(), "unexpected total")
+
+	c.Add(23)
+	require.Equal(t, uint64(23), c.Val(), "unexpected new value")
+	require.Equal(t, uint64(65), c.Total(), "unexpected new total")
+}
