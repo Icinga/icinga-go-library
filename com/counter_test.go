15703e68fdfce1724833d8af1b8ca977a6cfde71
Alexander Aleksandrovič Klimov <alexander.klimov@icinga.com>
2024-09-02 15:58:02 +0200

    Test `com.Counter` (#47)
    * Test com.Counter#Add()
    
    * Test com.Counter#Reset()
    
    * TestCounter_Reset(): check c.Reset() result
    
    Co-authored-by: alvar <8402811+oxzi@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: alvar <8402811+oxzi@users.noreply.github.com>

---
+++ com/counter_test.go
@@ -0,0 +1,32 @@
+package com
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestCounter_Add(t *testing.T) {
+	var c Counter
+
+	c.Add(42)
+	require.Equal(t, uint64(42), c.Val(), "unexpected value")
+	require.Equal(t, uint64(42), c.Total(), "unexpected total")
+
+	c.Add(23)
+	require.Equal(t, uint64(65), c.Val(), "unexpected new value")
+	require.Equal(t, uint64(65), c.Total(), "unexpected new total")
+}
+
+func TestCounter_Reset(t *testing.T) {
+	var c Counter
+
+	c.Add(42)
+	require.Equal(t, uint64(42), c.Reset(), "unexpected reset value")
+
+	require.Equal(t, uint64(0), c.Val(), "unexpected value")
+	require.Equal(t, uint64(42), c.Total(), "unexpected total")
+
+	c.Add(23)
+	require.Equal(t, uint64(23), c.Val(), "unexpected new value")
+	require.Equal(t, uint64(65), c.Total(), "unexpected new total")
+}
