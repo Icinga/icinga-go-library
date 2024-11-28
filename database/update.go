5bbcf6dfeccd273b5ea1d84f5e47a09076a5a028
Johannes Rauh <johannes.rauh@icinga.com>
2024-11-28 16:35:35 +0100

    Introduce database#UpdateStatement

---
+++ database/update.go
@@ -0,0 +1,64 @@
+package database
+
+type UpdateStatement interface {
+	SetTable(table string) UpdateStatement
+
+	SetSet(set string) UpdateStatement
+
+	SetWhere(where string) UpdateStatement
+
+	Entity() Entity
+
+	Table() string
+
+	Set() string
+
+	Where() string
+}
+
+func NewUpdate(entity Entity) UpdateStatement {
+	return &updateStatement{
+		entity: entity,
+	}
+}
+
+type updateStatement struct {
+	entity Entity
+	table  string
+	set    string
+	where  string
+}
+
+func (u *updateStatement) SetTable(table string) UpdateStatement {
+	u.table = table
+
+	return u
+}
+
+func (u *updateStatement) SetSet(set string) UpdateStatement {
+	u.set = set
+
+	return u
+}
+
+func (u *updateStatement) SetWhere(where string) UpdateStatement {
+	u.where = where
+
+	return u
+}
+
+func (u *updateStatement) Entity() Entity {
+	return u.entity
+}
+
+func (u *updateStatement) Table() string {
+	return u.table
+}
+
+func (u *updateStatement) Set() string {
+	return u.set
+}
+
+func (u *updateStatement) Where() string {
+	return u.where
+}
