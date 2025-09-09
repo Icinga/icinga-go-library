649cb701c5b1a5ebae202b4832c02a67a5c0d8ac
Alvar Penning <alvar.penning@icinga.com>
2025-09-09 11:47:54 +0200

    notifications: Move Rule Version and IDs to Event
    Remove the custom HTTP headers for the rules and their version and
    introduce them in the event.Event struct. This eases both serialization,
    deserialization and the HTTP code.

--- notifications/event/event_test.go
+++ notifications/event/event_test.go
@@ -18,19 +18,32 @@ func TestEvent(t *testing.T) {
 			t.Parallel()
 
 			event := &Event{
-				Name:     "TestEvent",
-				URL:      "example.com",
-				Tags:     map[string]string{"tag1": "value1"},
-				Type:     TypeState,
-				Severity: SeverityOK,
-				Username: "testuser",
-				Message:  "Test",
+				Name:         "TestEvent",
+				URL:          "example.com",
+				Tags:         map[string]string{"tag1": "value1"},
+				Type:         TypeState,
+				Severity:     SeverityOK,
+				Username:     "testuser",
+				Message:      "Test",
+				RulesVersion: "0x1",
+				RuleIds:      []int64{1, 2, 3, 6},
 			}
 
 			data, err := json.Marshal(event)
 			require.NoError(t, err)
 
-			expected := `{"name":"TestEvent","url":"example.com","tags":{"tag1":"value1"},"type":"state","severity":"ok","username":"testuser","message":"Test"}`
+			expected := `
+				{
+					"name":"TestEvent",
+					"url":"example.com",
+					"tags":{"tag1":"value1"},
+					"type":"state",
+					"severity":"ok",
+					"username":"testuser",
+					"message":"Test",
+					"rules_version": "0x1",
+					"rule_ids": [1, 2, 3, 6]
+				}`
 			assert.JSONEq(t, expected, string(data), "JSON encoding does not match expected output")
 		})
 
