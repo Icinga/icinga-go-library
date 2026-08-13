fa77ae929a31ccdec92ef3d0bb5a1d16900cb7ce
Jan Schuppik <jan.schuppik@icinga.com>
2026-08-13 16:23:01 +0200

    add method for transforming 2 byte slices in a UUID

--- types/uuid.go
+++ types/uuid.go
@@ -1,8 +1,11 @@
 package types
 
 import (
+	"bytes"
 	"database/sql/driver"
 	"encoding"
+	"encoding/binary"
+
 	"github.com/google/uuid"
 )
 
@@ -21,3 +24,23 @@ var (
 	_ encoding.TextUnmarshaler = (*UUID)(nil)
 	_ driver.Valuer            = UUID{}
 )
+
+func UUIDFromBinaries(a, b []byte) UUID {
+	buf := make([]byte, 0, 8+len(a)+8+len(b))
+
+	if bytes.Compare(a, b) > 0 {
+		a, b = b, a
+	}
+
+	var lenA [8]byte
+	binary.BigEndian.PutUint64(lenA[:], uint64(len(a)))
+	buf = append(buf, lenA[:]...)
+	buf = append(buf, a...)
+
+	var lenB [8]byte
+	binary.BigEndian.PutUint64(lenB[:], uint64(len(b)))
+	buf = append(buf, lenB[:]...)
+	buf = append(buf, b...)
+
+	return UUID{UUID: uuid.NewSHA1(uuid.Nil, buf)}
+}
